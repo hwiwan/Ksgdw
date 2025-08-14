842196a792a1f4a370ac17d4ecd2b13ce41970eb
kimhwiwan <khw97211@naver.com>
2025-08-14 17:05:22 +0900

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".youSwiper", {
+        navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".velSwiper", {
+        navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper = new Swiper(".roomSwiper", {
+        navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper = new Swiper(".bangSwiper", {
+        navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
